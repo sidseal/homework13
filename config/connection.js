88b12dea81958a352a19de6ba052de7152bc797b
sidseal <sidseal@me.com>
2020-06-10 00:46:58 -0400

    fix db

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
   port: 3306,
   user: "root",
   password: process.env.SQLpW,
-  database: "cat_db"
+  database: "burgers_db"
 });
 
 connection.connect(function(err) {
